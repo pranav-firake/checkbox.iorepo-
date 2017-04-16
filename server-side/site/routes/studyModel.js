b49cedd300936fa542ff3f060d95dbb56745403e
Sohan Kunkerkar <sohank2602@gmail.com>
2017-04-16 01:22:41 -0400

    Update studyModel.js

--- server-side/site/routes/studyModel.js
+++ server-side/site/routes/studyModel.js
@@ -41,11 +41,11 @@ SurveyModel = function( incoming, token )
 	self.markdown = incoming.markdown;
 	self.token = token;
 
-	self.adminLink = "http://54.186.125.106/studies/admin/?token=" + token;
+	self.adminLink = "http://54.218.91.168/studies/admin/?token=" + token;
 
 	self.setPublicLink = function(id)
 	{
-		self.publicLink = "http://54.186.125.106/studies/?id=" + id;
+		self.publicLink = "http://54.218.91.168/studies/?id=" + id;
 	};
 
 
@@ -78,11 +78,11 @@ DataStudyModel = function( incoming, token )
 	self.markdown = incoming.markdown;
 	self.token = token;
 
-	self.adminLink = "http://54.186.125.106/studies/admin/?token=" + token;
+	self.adminLink = "http://54.218.91.168/studies/admin/?token=" + token;
 
 	self.setPublicLink = function(id)
 	{
-		self.publicLink = "http://54.186.125.106/studies/?id=" + id;
+		self.publicLink = "http://54.218.91.168/studies/?id=" + id;
 	};
 
 
