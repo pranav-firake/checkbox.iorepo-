016d32106c21c74b84468fb32caccde9e2e5c01f
Pranav Firake <pranav.firake7@gmail.com>
2017-04-16 12:03:33 -0400

    Update studyModel.js

--- server-side/site/routes/studyModel.js
+++ server-side/site/routes/studyModel.js
@@ -41,11 +41,11 @@ SurveyModel = function( incoming, token )
 	self.markdown = incoming.markdown;
 	self.token = token;
 
-	self.adminLink = "http://54.202.221.133/studies/admin/?token=" + token;
+	self.adminLink = "http://54.149.148.211/studies/admin/?token=" + token;
 
 	self.setPublicLink = function(id)
 	{
-		self.publicLink = "http://54.202.221.133/studies/?id=" + id;
+		self.publicLink = "http://54.149.148.211/studies/?id=" + id;
 	};
 
 
@@ -78,11 +78,11 @@ DataStudyModel = function( incoming, token )
 	self.markdown = incoming.markdown;
 	self.token = token;
 
-	self.adminLink = "http://54.202.221.133/studies/admin/?token=" + token;
+	self.adminLink = "http://54.149.148.211/studies/admin/?token=" + token;
 
 	self.setPublicLink = function(id)
 	{
-		self.publicLink = "http://54.202.221.133/studies/?id=" + id;
+		self.publicLink = "http://54.149.148.211/studies/?id=" + id;
 	};
 
 
